46a956408547a9d23345cd6beea7c2134e0beb18
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-10-06 14:37:31 +0600

    fix some issue

--- i18n.tsx
+++ i18n.tsx
@@ -1,15 +1,15 @@
-import { getRequestConfig } from "next-intl/server"
-import { locales } from "./config"
-import { notFound } from "next/navigation"
+import { getRequestConfig } from 'next-intl/server';
+import { locales } from './config';
+import { notFound } from 'next/navigation';
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming locale is valid
   if (!locales.includes(locale as any)) {
-    notFound()
+    notFound();
   }
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default,
-  }
-})
-
+  };
+});
