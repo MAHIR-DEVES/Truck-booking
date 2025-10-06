e1b9e55cfe4459c6fb3cb9a325c23394ad3527a2
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-10-06 12:30:04 +0600

    install typescript

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import createMiddleware from "next-intl/middleware"
-import { locales, defaultLocale } from "./config"
+import createMiddleware from 'next-intl/middleware';
+import { locales, defaultLocale } from './config';
 
 export default createMiddleware({
   locales,
   defaultLocale,
-  localePrefix: "always",
-})
+  localePrefix: 'always',
+});
 
 export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
-} 
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+};
