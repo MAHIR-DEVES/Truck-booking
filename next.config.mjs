46fc827b48891bef071104c8748a7355586d708a
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-10-06 15:21:52 +0600

    agin deployment issue solved

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
-const withNextIntl = createNextIntlPlugin('./i18n.tsx');
- 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
-export default withNextIntl(nextConfig);
 
+const withNextIntl = createNextIntlPlugin('./i18n.tsx');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add webpack configuration to handle the module issue
+  webpack: config => {
+    // Fix for the webpack modules issue
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+
+    return config;
+  },
+};
+
+export default withNextIntl(nextConfig);
